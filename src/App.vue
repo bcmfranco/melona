2c73df65241862e18354d8be6c913e6edb48e947
bcmfranco <bruno@codev.com.ar>
2024-02-03 21:43:23 -0300

    add melona module

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <MelonasGreeting />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MelonasGreeting from './components/MelonasGreeting.vue'
+
+
+let count = 0;
+
+</script>
 
 <style scoped>
 .logo {
---
+++ src/components/MelonasGreeting.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <button type="button" @click="generateGreeting">Saludar</button>
+    <p>{{ greeting }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let greeting = ref('');
+let showGreeting = ref(false);
+let timerId;
+
+const generateGreeting = () => {
+  greeting.value = '¡Hola Melona!';
+  showGreeting.value = true;
+
+  clearTimeout(timerId);
+  timerId = setTimeout(() => {
+    showGreeting.value = false;
+  }, 2000);
+};
+</script>
+
+<style scoped>
+    .read-the-docs {
+    color: #888;
+    }
+    
+</style>
